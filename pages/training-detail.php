4db46275227aaf0e08090f5814f4105d8a474bc9
Mofisam <malaosamuel2020@gmail.com>
2026-06-27 13:27:54 +0100

    training image size ajusted

--- pages/training-detail.php
+++ pages/training-detail.php
@@ -262,7 +262,7 @@ $pageDescription = $training['short_description'];
                         <img src="../assets/images/trainings/<?php echo htmlspecialchars($training['featured_image']); ?>" 
                              class="img-fluid rounded" 
                              alt="<?php echo htmlspecialchars($training['title']); ?>"
-                             style="max-height: 400px; width: 100%; object-fit: cover;">
+                             style="height: 400px; width: 100%; object-fit: cover;">
                     </div>
                 </div>
                 <?php endif; ?>
@@ -368,7 +368,7 @@ $pageDescription = $training['short_description'];
                                         <img src="../assets/images/trainings/<?php echo htmlspecialchars($related['featured_image']); ?>" 
                                              class="card-img-top" 
                                              alt="<?php echo htmlspecialchars($related['title']); ?>"
-                                             style="height: 500px; object-fit: cover;">
+                                             style="height: 120px; object-fit: cover;">
                                     <?php else: ?>
                                         <div class="card-img-top bg-primary text-white d-flex align-items-center justify-content-center" 
                                              style="height: 120px;">
